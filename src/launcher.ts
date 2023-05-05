e78e67d88a5ec37566e0107d3b12bfaf6ce6d414
ManZill <77339213+ManZill@users.noreply.github.com>
2023-05-05 08:21:00 +0400

    Создана начальная программа (#1)
    * Добавлены конфигурационные файлы
    Настроены стандартные скрипты и зависимости
    Добавлен модуль окна
    
    * Исправлены игноры файлов/директорий
    
    ---------
    
    Co-authored-by: Matvei Zhukov <matvei.zkuk@gmail.com>

---
+++ src/launcher.ts
@@ -0,0 +1,28 @@
+import {app, BrowserWindow} from "electron";
+import * as path from "path";
+
+function createWindow() {
+    let mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    })
+
+    mainWindow.loadFile(path.join(__dirname, "../index.html")).then();
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+})
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
